d05e1e06acd935c05fa954e1413cc3a5b7ce9722
unknown <lakheraakshay@gmail.com>
2021-10-20 13:53:30 +0530

    done 9

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let express = require("express");
 let app = express();
 let server = require("http").Server(app);
 let io = require("socket.io")(server);
-let stream = require("./src/ws/stream");
+let stream = require("src/ws/stream");
 let path = require("path");
 let favicon = require("serve-favicon");
 const PORT = process.env.PORT || 3000;
@@ -17,7 +17,6 @@ app.use("/assets", express.static(path.join(__dirname, "src", "assets")));
 console.log(__dirname);
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/src/index.html");
-  
 });
 
 io.of("/stream").on("connection", stream);
